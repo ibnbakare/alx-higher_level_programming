a4751d9fc066fe5ed59a860398de3e2cfec41394
ridhwan bakare <ridmartrite@gmail.com>
2022-09-19 08:26:05 -0700

    adding 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = str1 + " " + str2
+str1 = str1 + " " + str2
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"Welcome to {str3}!")
+print(f"Welcome to {str1}!")
