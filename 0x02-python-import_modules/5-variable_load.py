883dbb3e11210cf4e97ead1e8e335a5b15bba835
ridhwan bakare <ridmartrite@gmail.com>
2022-09-22 07:51:47 -0700

    adding task 5 again

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import variable_load_5
+from variable_load_5 import a
 if __name__ == "__main__":
-    b = variable_load_5.a
-    print("{}".format(b))
+    print("{}".format(a))
