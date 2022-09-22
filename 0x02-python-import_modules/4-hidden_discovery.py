540b43e67c22ab0270750ecf41788f72fd5dce4f
ridhwan <ridmartrite@gmail.com>
2022-09-22 15:00:10 +0100

    adding task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    for d in dir(hidden_4):
-        if "__" not in d:
-            print(d)
-            
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != "_":
+            print("{}".format(name))
