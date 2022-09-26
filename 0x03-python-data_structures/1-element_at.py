396c82c6a1e3e3a79f69b57ac1911010f3867c9b
ridhwan bakare <ridmartrite@gmail.com>
2022-09-26 02:46:10 -0700

    adding again

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    count = 1
     length = len(my_list)
-    if idx < 0 or idx > int(length) -1:
+    if (idx < 0 or idx > int(length) - 1):
         return (None)
     return (my_list[idx])
 
+
 if __name__ == "__main__":
-    print(element_at([1,3], 0))
+    print(element_at([1, 3], 0))
