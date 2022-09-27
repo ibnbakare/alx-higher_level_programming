e5a6b9d57b6c090f57bc455e35273c982e149d19
ridhwan bakare <ridmartrite@gmail.com>
2022-09-27 02:24:45 -0700

    adding task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_interger(my_list=[]):
-    for r in my_list:
-        print("{:d}".format(r))
+    if my_list:
+        for r in my_list:
+            print("{:d}".format(r))
