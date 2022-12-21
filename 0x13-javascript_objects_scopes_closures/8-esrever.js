ef2fd939171fa22a0a7a2f78854b04272cc68191
Ridhwan Bakare <67150631+ibnbakare@users.noreply.github.com>
2022-12-21 04:50:22 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let myList = [];
-  for (let i = list.length - 1; i > -1; i--) {
-    myList.push(list[i]);
-  }
-  return myList;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
