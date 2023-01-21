56691da5f2f528e75adac3954b40e2b7c5508579
ridhwan <ridmartrite@gmail.com>
2023-01-21 14:30:52 -0500

    adding task 5 again

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    req = requests.get("https://alx-intranet.hbtn.io/status")
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
