a49e282e5989d8452acdeb681ab79d3195f8c424
ridhwan bakare <ridmartrite@gmail.com>
2023-02-11 01:14:43 -0500

    adding task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,26 @@
 #!/usr/bin/node
 
-const req = require("request");
+const request = require('request');
 const url = process.argv[2];
 
-req.get(url,function(err,res,body){
-    if (err){
-        console.log(err)
-    }
-    else {
-        jobs = JSON.parse(body)
-        console.log(jobs.length)
-            for (let i = 0; i < jobs.length; i++){
-             if (jobs[i]['compeleted'] === true){
-                console.log(jobs)
-            }
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
         }
+      }
     }
-})
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
