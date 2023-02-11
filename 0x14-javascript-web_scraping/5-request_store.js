2c94dd0486cb4c913a79ce4ce870e77e1fc71bbe
ridhwan bakare <ridmartrite@gmail.com>
2023-02-11 01:41:06 -0500

    adding task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
-let fs = require('fs');
-let request = require('request');
-let url = process.argv[2];
-let path = process.argv[3];
-
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(body)
-    fs.writeFile(path, body, 'utf8', function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
